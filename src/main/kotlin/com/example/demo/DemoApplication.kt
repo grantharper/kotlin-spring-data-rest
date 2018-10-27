9b685c7a65c0ef6a55217875452275eabc7293a6
grantharper <grant.e.harper@gmail.com>
2018-10-27 09:05:31 -0400

    consolidate source code files into one

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,6 +2,13 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 @SpringBootApplication
 class DemoApplication
@@ -10,3 +17,12 @@ fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
 
+@Entity
+data class Person(@Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long,
+                  var firstName: String, var lastName: String)
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+interface PersonRepository : PagingAndSortingRepository<Person, Long> {
+
+    fun findByLastName(@Param("name") name: String): List<Person>
+}
